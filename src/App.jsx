1ecdaeb75e34d06ed194dc2476425205fe02b2e9
Bayu157 <aichigaming2@gmail.com>
2025-02-08 23:42:08 +0700

    Fix: Layout Grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-
-import Navbar from "./Layout/Navbar"
+import Header from "./Layout/Header"
 import Sidebar from "./Layout/Sidebar"
+import SeriesCard from "./Components/SeriesCard"
 import SidebarContextProvider from "./Context/SidebarContext"
 import { BrowserRouter } from "react-router-dom"
 
@@ -10,10 +10,20 @@ function App() {
     <>
       <BrowserRouter>
         <SidebarContextProvider>
-          <Navbar></Navbar>
-          <Sidebar></Sidebar>
+          <div className="grid grid-cols-6 grid-rows-10 gap-4 h-screen">
+            <div className="col-span-6">
+              <Header></Header>
+            </div>
+            <div className="row-span-10">
+              <Sidebar></Sidebar>
+              <div className=" "> 
+                <div className="bg-gray-100"></div>
+              </div>
+            </div>
+          </div>
         </SidebarContextProvider>
       </BrowserRouter>
+    
     </>    
   )
 }
